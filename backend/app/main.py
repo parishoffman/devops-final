b00c51bd00546910b9af09a5f35fb97aaf263522
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-05-06 11:43:36 -0700

    fix: update cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 # Enable CORS for the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
