002d42426ef2ed9d982936d494f5c034a952f28d
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-05-06 12:50:30 -0700

    feat: spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
     console.log(`REACT_APP_API_URL: ${REACT_APP_API_URL}`)
 
 
+    
     fetch(`${REACT_APP_API_URL}/visits`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
